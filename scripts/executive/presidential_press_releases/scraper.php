b2ca0ed9a08413d72e0864f36ea37a005aa2e5ef
Chauncey Thorn <chaunceyt@gmail.com>
2009-08-20 00:36:01 -0400

    initial fix issue #49 - review for better fix

--- scripts/executive/presidential_press_releases/scraper.php
+++ scripts/executive/presidential_press_releases/scraper.php
@@ -48,7 +48,7 @@ class PresidentialPressReleases extends EventScraper_Abstract
         $total_timestamps = sizeof($data_arr[0]['timestamp']);
         for($i=0; $i < $total_timestamps; $i++) {
             preg_match('#<a[^>]*>(.*?)</a>#is', $data_arr[0]['description'][$i], $title);
-            $events[$i]['couchdb_id'] = (string) $this->_vd_date_format($data_arr[0]['timestamp'][$i]) . ' - '.BranchName::$executive.' - '.EntityName::$whitehouse.' - '. trim($title[1]);
+            $events[$i]['couchdb_id'] = (string) $this->_vd_date_format($data_arr[0]['timestamp'][$i]) . ' - '.BranchName::$executive.' - '.EntityName::$whitehouse.' - '. str_replace('\'','&#039;',trim($title[1]));
             $events[$i]['datetime'] = (string) $this->_vd_date_format($data_arr[0]['timestamp'][$i]);
             $events[$i]['end_datetime'] = null;
             $events[$i]['title'] = (string) trim($title[1]);
